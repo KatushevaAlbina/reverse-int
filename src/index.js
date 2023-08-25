d0c2775a140c9b51311feb67f831abdfed1aacee
Katushev Albina <atuktarova83@gmail.com>
2023-08-25 15:13:47 +0300

    solved the reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function reverse (n) {
-  
+/*module.exports = function reverse (n) {
+for(var r = 0; n; n = Math.floor(n / 10)) {
+        r *= 10;
+        r += n % 10;
+    }
+    return r;
 }
+
+
+module.exports = function reverse (n) {
+    let newArr = String(n);
+    let result = newArr.split('').reverse().join('');
+    return +result;
+}  НЕ правильное решение
+*/
+
+module.exports = function reverse (n) {
+let newArr = String(Math.abs(n));
+    let result = newArr.split('').reverse().join('');
+    return +result;
+}
+
+
+
+
